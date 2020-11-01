6339c7ca64f92b365b806ab2b41e59682d6aebca
med amine blibech <56481634+med363@users.noreply.github.com>
2020-11-01 14:55:22 +0200

    Update server.js

--- ServerR/server.js
+++ ServerR/server.js
@@ -1,87 +1,43 @@
 const express = require("express");
 const assert = require("assert");
 const bodyParser  = require('body-parser');
-const { MongoClient, ObjectID } = require("mongodb");
 const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
-const Mongo_url = "mongodb://localhost:27017";
-const dbName = "Municipalite-api";
 // get our request parameters
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+const port = 5001
 
 
-
-MongoClient.connect(Mongo_url, { useUnifiedTopology: true }, (err, client) => {
-  assert.equal(err, null, "DATA FAILED");
-  const db = client.db(dbName);
-  console.log("db is connected");
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+//database configuration
+var MongoClient = require('mongodb').MongoClient;
+// Connect to the db
+MongoClient.connect("mongodb://localhost:27017/Municipalite",{useUnifiedTopology: true},(err, client)  => {
   
+  if (err) return console.error(err)
+  console.log('Connected to Database')
+  const db = client.db('Municipalité')
+  const employeesCollection = db.collection('employees')
+  app.post('/voyage', (req, res) => {
+    employeesCollection.insertOne(req.body)
+      .then(result => {
+        console.log(result)
+        res.send(result)
+      })
+      .catch(error => console.error(error))
+  })
+  app.get('/voyage', (req, res) => {
+    db.collection('employees').find().toArray()
+      .then(result => {
+        console.log(result)
+        res.json(result)
+      })
+      .catch(error => console.error(error))
+  })
+})
 
-  ///contact collection
-  app.post("/contacts1", (req, res) => {
-    const new_elem = req.body;
-    db.collection("contacts&problems").insertOne(new_elem, (err, data) => {
-      if (err) {
-        res.status(400).json({ error: "Error contact not inserted" });
-      } else {
-        res.json({ success: data.insertedId });
-      }
-    });
-  });
-  app.get("/contacts1", (req, res) => {
-    //  let  header=ObjectID(req.params.id)
-    db.collection("contacts&problems")
-      .find()
-      .toArray((err, data) => {
-        if (err) res.send("not found");
-        else res.send(data);
-      });
-  });
-});
-//affichINFO collection
-app.post("/voyage", (req, res) => {
-  const new_elem = req.body;
-  db.collection("voyage").insertOne(new_elem, (err, data) => {
-    if (err) {
-      res.status(400).json({ error: "Error voyage not inserted" });
-    } else {
-      res.json({ success: data.insertedId });
-    }
-  });
-});
-app.get("/voyage", (req, res) => {
-  //  let  header=ObjectID(req.params.id)
-  db.collection("voyage")
-    .find()
-    .toArray((err, data) => {
-      if (err) res.send("not found");
-      else res.send(data);
-    });
-});
-//poseQUESTION COLLECTION
-app.post("/question", (req, res) => {
-  const new_elem = req.body;
-  db.collection("question").insertOne(new_elem, (err, data) => {
-    if (err) {
-      res.status(400).json({ error: "Error question not inserted" });
-    } else {
-      res.json({ success: data.insertedId });
-    }
-  });
-});
-app.get("/question", (req, res) => {
-  //  let  header=ObjectID(req.params.id)
-  db.collection("question")
-    .find()
-    .toArray((err, data) => {
-      if (err) res.send("not found");
-      else res.send(data);
-    });
-});
-app.listen(5001, (err) => {
-  if (err) console.log("errr");
-  else console.log("server run on 5001");
-});
